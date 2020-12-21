4403e2262b357e522f51b83d4a25d94b5dbe644d
wiekiang <wiekiang.hi@cqumail.com>
2020-12-21 17:08:26 +1000

    generate the output

--- model.py
+++ model.py
@@ -44,14 +44,6 @@ def create_pairs_on_set(images, labels):
     return pairs, y
 
 
-def show_image(image):
-    plt.figure()
-    plt.imshow(image)
-    plt.colorbar()
-    plt.grid(False)
-    plt.show()
-
-
 # Download and prepare train and test sets. 
 # Create pairs of images that will go into the multi-input model.
 
@@ -151,51 +143,13 @@ test_accuracy = compute_accuracy(ts_y, y_pred_test)
 
 print("Loss = {}, Train Accuracy = {} Test Accuracy = {}".format(loss, train_accuracy, test_accuracy))
 
-def plot_metrics(metric_name, title, ylim=5, file):
+def plot_metrics(metric_name, title, ylim=5):
     plt.title(title)
     plt.ylim(0,ylim)
     plt.plot(history.history[metric_name],color='blue',label=metric_name)
     plt.plot(history.history['val_' + metric_name],color='green',label='val_' + metric_name)
-    plt.savefig(file)
+    plt.savefig('loss.png')
 
 
-plot_metrics(metric_name='loss', title="Loss", ylim=0.2, 'loss.png')
+plot_metrics(metric_name='loss', title="Loss", ylim=0.2)
 
-# Matplotlib config
-def visualize_images():
-    plt.rc('image', cmap='gray_r')
-    plt.rc('grid', linewidth=0)
-    plt.rc('xtick', top=False, bottom=False, labelsize='large')
-    plt.rc('ytick', left=False, right=False, labelsize='large')
-    plt.rc('axes', facecolor='F8F8F8', titlesize="large", edgecolor='white')
-    plt.rc('text', color='a8151a')
-    plt.rc('figure', facecolor='F0F0F0')# Matplotlib fonts
-
-
-# utility to display a row of digits with their predictions
-def display_images(left, right, predictions, labels, title, n):
-    plt.figure(figsize=(17,3))
-    plt.title(title)
-    plt.yticks([])
-    plt.xticks([])
-    plt.grid(None)
-    left = np.reshape(left, [n, 28, 28])
-    left = np.swapaxes(left, 0, 1)
-    left = np.reshape(left, [28, 28*n])
-    plt.imshow(left)
-    plt.figure(figsize=(17,3))
-    plt.yticks([])
-    plt.xticks([28*x+14 for x in range(n)], predictions)
-    for i,t in enumerate(plt.gca().xaxis.get_ticklabels()):
-        if predictions[i] > 0.5: t.set_color('red') # bad predictions in red
-    plt.grid(None)
-    right = np.reshape(right, [n, 28, 28])
-    right = np.swapaxes(right, 0, 1)
-    right = np.reshape(right, [28, 28*n])
-    plt.plot(right)
-    plt.savefig('results.png')
-
-
-y_pred_train = np.squeeze(y_pred_train)
-indexes = np.random.choice(len(y_pred_train), size=10)
-display_images(tr_pairs[:, 0][indexes], tr_pairs[:, 1][indexes], y_pred_train[indexes], tr_y[indexes], "clothes and their dissimilarity", 10)
